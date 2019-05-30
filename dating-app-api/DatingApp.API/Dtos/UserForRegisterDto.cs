386814597a3a40c4d6253f32ff4da3718585d6eb
willianfalbo <willianfalbo@gmail.com>
2019-05-29 23:22:39 -0300

    module5 - Error handling for UI and API

--- dating-app-api/DatingApp.API/Dtos/UserForRegisterDto.cs
+++ dating-app-api/DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -8,7 +8,7 @@ namespace DatingApp.API.Dtos
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters.")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify a password between 4 and 8 characters.")]
         public string Password { get; set; }
     }
 }
--- dating-app/src/app/app.module.ts
+++ dating-app/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
+import { AuthService } from './_services/auth.service';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-
-import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -23,7 +24,8 @@ import { RegisterComponent } from './register/register.component';
       FormsModule
    ],
    providers: [
-      AuthService
+      AuthService,
+      ErrorInterceptorProvider,
    ],
    bootstrap: [
       AppComponent
