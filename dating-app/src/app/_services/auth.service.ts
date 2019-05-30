ce6d7c575983638a379d3ae6abda49ff47fd5de0
willianfalbo <willianfalbo@gmail.com>
2019-05-30 00:44:28 -0300

    configured environments for the Angular application

--- dating-app/src/app/_services/auth.service.ts
+++ dating-app/src/app/_services/auth.service.ts
@@ -2,18 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { map } from 'rxjs/operators';
+import { DATINGAPP_API } from '../app.api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  baseUrl = 'http://localhost:5000/api/auth';
-
   constructor(private http: HttpClient) { }
 
   login(model: any) {
-    return this.http.post(`${this.baseUrl}/login`, model).pipe(
+    return this.http.post(`${DATINGAPP_API}/auth/login`, model).pipe(
       map((response: any) => {
         if (response) {
           localStorage.setItem('token', response.token);
@@ -23,7 +22,7 @@ export class AuthService {
   }
 
   register(model: any) {
-    return this.http.post(`${this.baseUrl}/register`, model);
+    return this.http.post(`${DATINGAPP_API}/auth/register`, model);
   }
 
 }
