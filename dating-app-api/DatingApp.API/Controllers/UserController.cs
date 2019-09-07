47e35eab6e5acae359c5e3209e1eb30943e5c947
willianfalbo <willianfalbo@gmail.com>
2019-09-07 00:47:11 -0300

    module13 - Backend - Created Action Filter to Log User Activity

--- dating-app-api/DatingApp.API/Controllers/UserController.cs
+++ dating-app-api/DatingApp.API/Controllers/UserController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [Authorize]
     [Route("api/users")]
     [ApiController]
--- dating-app-api/DatingApp.API/Startup.cs
+++ dating-app-api/DatingApp.API/Startup.cs
@@ -69,6 +69,9 @@ namespace DatingApp.API
                         ValidateAudience = false,
                     };
                 });
+            
+            // to log any user activity
+            services.AddScoped<LogUserActivity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
